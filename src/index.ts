4662c205a7d49feaed4681ba9906ee54db197a5e
Manoel Valladao <valladao@gmail.com>
2024-09-22 15:23:59 -0300

    feat: Update GraphQL API version to unstable

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,49 @@
-import dotenv from 'dotenv';
-import { ApolloClient, InMemoryCache, gql, HttpLink } from '@apollo/client/core';
-import fetch from 'node-fetch';
+import dotenv from "dotenv"
+import { ApolloClient, InMemoryCache, gql, HttpLink } from "@apollo/client/core"
+import fetch from "node-fetch"
 
-dotenv.config();
+dotenv.config()
 
 const httpLink = new HttpLink({
-  uri: `https://${process.env.SHOPIFY_SHOP_NAME}/admin/api/2023-04/graphql.json`,
+  uri: `https://${process.env.SHOPIFY_SHOP_NAME}/admin/api/unstable/graphql.json`,
   fetch: fetch as any,
   headers: {
-    'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN || '',
-  },
-});
+    "X-Shopify-Access-Token": process.env.SHOPIFY_ACCESS_TOKEN || ""
+  }
+})
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache(),
-});
+  cache: new InMemoryCache()
+})
 
 const GET_BLOG_POSTS = gql`
   query GetBlogPosts {
-    blogs(first: 1) {
+    blogs(first: 250) {
       edges {
         node {
-          id
           title
-          articles(first: 10) {
-            edges {
-              node {
-                id
-                title
-                content
-                publishedAt
-              }
-            }
-          }
         }
       }
     }
   }
-`;
+`
 
 async function fetchBlogPosts() {
   try {
     const { data } = await client.query({
-      query: GET_BLOG_POSTS,
-    });
+      query: GET_BLOG_POSTS
+    })
 
     if (data.blogs.edges.length > 0) {
-      const articles = data.blogs.edges[0].node.articles.edges;
-      console.log(JSON.stringify(articles, null, 2));
+      const articles = data.blogs.edges[0].node.articles.edges
+      console.log(JSON.stringify(articles, null, 2))
     } else {
-      console.log('No blogs found');
+      console.log("No blogs found")
     }
   } catch (error) {
-    console.error('Error fetching blog posts:', error);
+    console.error("Error fetching blog posts:", error)
   }
 }
 
-fetchBlogPosts();
+fetchBlogPosts()
